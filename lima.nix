b06477a625a4ef512da8bb026da42f5723a3d9a4
Sean Gilligan <sean@msgilligan.com>
2026-06-18 15:06:53 -0700

    lima.nix: explicitly set users.mutableUsers = true
    This is not a change from the default, but we should document that
    it is a setting we require.

--- lima.nix
+++ lima.nix
@@ -20,6 +20,11 @@
   # This simplifies remote deployment to the instance's nix store.
   nix.settings.trusted-users = [ "@wheel" ];
 
+  # lima-init imperatively adds a user at startup. `users.mutableUsers` should be `true`
+  # to prevent `nixos-rebuild` from overwriting that user which can cause login to fail.
+  # The default is `true`, but we'll set it explicitly to document our requirement.
+  users.mutableUsers = true;
+
   # Read Lima configuration at boot time and run the Lima guest agent
   services.lima.enable = true;
 
