88a54da483095510079173f091fc73a919995269
Sean Gilligan <sean@msgilligan.com>
2026-06-18 15:05:33 -0700

    make-release.sh: Use "Pending release" for placeholder release notes

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -4,7 +4,8 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 NIXOS_LIMA_TAG=$1
+
 gh release create $NIXOS_LIMA_TAG \
   --title "Release $NIXOS_LIMA_TAG" \
-  --notes "Test release" \
+  --notes "Pending release" \
   --prerelease  
