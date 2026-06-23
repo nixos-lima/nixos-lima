75856f75c522d4ce04cb9d48f4908f876f0aaef4
Sean Gilligan <sean@msgilligan.com>
2026-06-22 22:46:36 -0700

    (TEST) flake.nix: use nixos-unstable-small (for Lima v2.1.3)
    This is a test branch to use Lima 2.1.3 which is currently available on
    nixos-unstable-small.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-26.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
